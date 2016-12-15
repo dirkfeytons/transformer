1a7d19eadc47c217507b79b5a896db1e5ec16d16
Dirk Feytons <dirk.feytons@technicolor.com>
2016-12-15 23:09:29 +0100

    ubus mapper: decrease timeout and output trace if timeout occurs
    Set the timeout of a ubus operation explicitly to 2 seconds instead of the
    default 30 seconds to reduce the impact of a non-responding ubus service.
    Should a timeout occur on a call then output a trace with information.

--- transformer/mapper/ubus.lua
+++ transformer/mapper/ubus.lua
@@ -29,11 +29,15 @@ See LICENSE file for more details.
 -- For event sending we need to use a different connection.
 -- Otherwise somebody listening for that event on the same
 -- connection will not receive it (!!).
+local logger = require("transformer.logger")
 local conn
 local event_conn
 do
   local ubus = require("ubus")
-  conn = ubus.connect()
+  -- Override the default UBUS call timeout of 30 seconds to 2 seconds.
+  -- If a UBUS call happens to a daemon while it is being started or stopped,
+  -- this call can hang causing all other Transformer calls to hang as well.
+  conn = ubus.connect(nil, 2)
   event_conn = ubus.connect()
   if not conn or not event_conn then
     error("Failed to connect to ubusd")
@@ -41,6 +45,17 @@ do
   local __index = getmetatable(conn).__index
   __index.close = function()
     -- just ignore a close request
+  end
+  -- override the call() method to be able to log a message when
+  -- a timeout happens
+  local call = __index.call
+  __index.call = function(_, path, method, ...)
+    local ret, rv = call(conn, path, method, ...)
+    -- Check ubusmsg.h for the possible return code values
+    if not ret and tonumber(rv) == 7 then
+      logger:warning("ubus call timeout on calling %s %s. %s", path, method, debug.traceback())
+    end
+    return ret, rv
   end
   -- override the send() method to
   -- use the dedicated event connection
